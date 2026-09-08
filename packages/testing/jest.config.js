fc078a1f8c89a9d542fccd0dd236a2c4702b2168
Aakif-Kohari <kohariaakif@gmail.com>
2026-09-08 17:53:37 +0000

    chore(testing): scaffold packages/testing with tsconfig, jest config, empty barrel

---
+++ packages/testing/jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  testEnvironment: 'node',
+  passWithNoTests: true,
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.test.json',
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/index.ts'],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
+};
